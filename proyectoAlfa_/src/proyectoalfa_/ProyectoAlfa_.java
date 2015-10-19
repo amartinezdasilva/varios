da886c35c13757164da0094be55fba9ef283c62c
amartinezdasilva <amartinezdasilva@atanasoff22.danielcastelao.org>
2015-10-19 09:19:33 +0200

    proyecto alfa

---
+++ proyectoAlfa_/src/proyectoalfa_/ProyectoAlfa_.java
@@ -0,0 +1,19 @@
+
+package proyectoalfa_;
+
+
+public class ProyectoAlfa_ {
+
+   
+    public static void main(String[] args) {
+       float multiplicacion ,suma, resta ,num1, num2;
+        num1 = 5;
+        num2 = 2;        
+        suma = num1 +num2;       
+        resta = num1 -num2;
+        multiplicacion = num1*num2;
+        System.out.println("suma =" + suma);
+        System.out.println("resta =" + resta);
+    }
+    
+}
